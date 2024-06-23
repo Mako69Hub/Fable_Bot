c6222200add655c9062d944feb3d2d1eb9762faa
Mako <skb@gmail.com>
2024-06-23 14:56:39 +0300

    пуск бота

---
+++ run.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from confige import TOKEN
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit bot')
